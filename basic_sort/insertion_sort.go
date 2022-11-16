2d22025eedd7abd6388be3d4ea26e478a1360339
Arsham <Arshamalh@gmail.com>
2022-11-16 13:49:58 +0330

    Add: insertion sort.

---
+++ basic_sort/insertion_sort.go
@@ -0,0 +1,34 @@
+package basicsort
+
+// One of the famous sorting algorithms
+func InsertionSort(array []int) []int {
+	for i := 1; i < len(array); i++ {
+		currentVal := array[i]
+		last_node := 0
+		for j := i - 1; j >= 0; j-- {
+			if array[j] > currentVal {
+				array[j+1] = array[j]
+			} else {
+				last_node = j + 1
+				break
+			}
+		}
+		array[last_node] = currentVal
+	}
+	return array
+}
+
+// Array type for sorted insert
+type Array []int
+
+// Sorted Insert algorithm which inserts new items in order.
+func (arr *Array) SortedInsert(item int) {
+	*arr = append(*arr, item)
+	for i := len(*arr) - 2; 0 <= i; i-- {
+		current := (*arr)[i]
+		if item < current {
+			(*arr)[i+1] = current
+			(*arr)[i] = item
+		}
+	}
+}
