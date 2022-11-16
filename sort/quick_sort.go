f52b4dbf15125d737d537a01b60c3e0c100bd5fe
Arsham <Arshamalh@gmail.com>
2022-11-16 21:05:15 +0330

    Doc: Important tip.

--- sort/quick_sort.go
+++ sort/quick_sort.go
@@ -13,6 +13,9 @@ func QuickSort(array []int) []int {
 }
 
 // Divider will select a pivot number and order fewer and higher numbers on the left and right side of it.
+//
+// Important Note: As we're using 0 for pp, we also used it for keeping count, but if pp initial value is not 0,
+// we should use two separate variables.
 func Divider(array []int) (pp int, left []int, right []int) {
 	for i := 1; i < len(array); i++ {
 		if array[i] <= array[0] {
@@ -22,4 +25,20 @@ func Divider(array []int) (pp int, left []int, right []int) {
 	}
 	array[0], array[pp] = array[pp], array[0]
 	return pp, array[:pp], array[pp+1:]
+}
+
+func DividerPivot(array []int, pp int) (left []int, right []int) {
+	count := 0
+	for i := 0; i < len(array); i++ {
+		if i == pp {
+			continue
+		}
+		if array[i] <= array[pp] {
+			count++
+			array[count], array[i] = array[i], array[count]
+		}
+	}
+	// TODO: Not working, needs more work around it.
+	array[pp], array[count] = array[count], array[pp]
+	return array[:count], array[count+1:]
 }
